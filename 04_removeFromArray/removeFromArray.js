46faebfa620d84422f979fe6d1fa9b81ac4a6dcd
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-28 00:06:16 -0400

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...terms) {
 
+    let newArr;
+    for (let i = 0; i < terms.length; i++) {
+        newArr = [];
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] !== terms[i]) {
+                newArr.push(arr[j]);
+            }
+        }
+        arr = newArr;
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
