eb2aa59b1e157ab62876d3cc743d3eda9b5d7b15
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-28 11:10:42 -0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if (typeof num1 == "number" && typeof num2 == "number") {
+        if (num1 >= 0 && num2 >= 0) {
+            if (num2 > num1) {
+                for (let i = num1; i <= num2; i++) {
+                    sum += i;
+                }
+            } else {
+                for (let i = num2; i <= num1; i++) {
+                    sum += i;
+                }
+            }    
+            return sum;        
+        }
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
