6b8cd69c5bb4bcb1f573c9b1105d0094ffaef74b
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-27 23:42:22 -0400

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    arr = arr.reverse();
+    const newString = arr.join('');
+    return newString
 };
 
 // Do not edit below this line
