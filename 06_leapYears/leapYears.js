f86a608041b7ce7bc1bc1292f26c976214f51a34
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-28 11:19:37 -0400

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear = false;
+    if (year % 4 === 0 && year % 100 !== 0) {
+        leapYear = true;
+    } else if (year % 400 === 0) {
+        leapYear = true;
+    }
 
+    return leapYear;
 };
 
 // Do not edit below this line
