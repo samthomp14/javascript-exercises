703a9f5adb2fef4db91140b1ded51d5eb2875dc7
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-31 12:44:18 -0400

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if (person.yearOfDeath) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return (new Date().getFullYear()) - person.yearOfBirth;
+    }
+}
+    
 
+const findTheOldest = function(people) {
+    return people.reduce(function(oldestPerson, currPerson) {
+        if (!oldestPerson.name) {
+            oldestPerson = currPerson;
+        } else {
+            const oldestAge = getAge(oldestPerson);
+            const currAge = getAge(currPerson);
+            if (currAge > oldestAge) {
+                oldestPerson = currPerson;
+            }
+        }
+        return oldestPerson;
+    }, {})
 };
 
 // Do not edit below this line
