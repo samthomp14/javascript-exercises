5386aad334859481326f2fcfe93793547836f67e
Samuel Thompson <134101570+samthomp14@users.noreply.github.com>
2023-05-31 12:23:41 -0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 0) return 'OOPS'
+    let prev = 1;
+    let curr = 0;
+    let fib = 0;
+    for (let i = 0; i < n; ++i) {
+        fib += prev;
+        prev = curr;
+        curr = fib;
+    }
 
+    return fib;
 };
 
 // Do not edit below this line
